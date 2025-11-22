e77d186b7b1fcef89f03177d3252d651b8fac6d3
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-11-22 23:56:42 +0530

    working with   prisma and postgres

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 import Image from "next/image";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ui/ModeToggle";
+
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-              <SignInButton  mode="modal">
-                <Button className="m-4" variant="default">Sign In</Button>
-              </SignInButton>
-              <SignUpButton mode="modal">
-                <Button className="mt-4" variant="secondary">Sign Up</Button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-            <ModeToggle />
+      <h1>Home page content</h1>
     </div>
-    
   );
 }
 
