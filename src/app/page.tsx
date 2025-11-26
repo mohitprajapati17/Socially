ae2d8ecab4cb48361951941bf493ef60d143b010
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-11-26 23:57:07 +0530

    adding all feature of post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,29 @@ import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@cl
 import prisma from "@/lib/prisma";
 import CreatePost from "@/components/CreatePost";
 import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/syncUser";
+import PostCard from "@/components/PostCard";
 
 export default async function Home() {
-  
+  const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
   return (
-    <div className ="grid grid-cols-1 lg:grid-cols-10 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
-        <CreatePost/>
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
       </div>
-      <div className ="hidden lg:block lg:col-span-4 sticky top-20">
-       <WhoToFollow/>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
       </div>
     </div>
   );
